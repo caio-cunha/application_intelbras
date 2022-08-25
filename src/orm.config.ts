0ae511cc6d49667fde8afbe48e125bacf4daca27
caio-cunha <caiocomputacao2014@gmail.com>
2022-08-25 19:04:56 -0300

    fix

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -3,11 +3,11 @@ import { VideoDevice } from './videodevices/entities/videodevice.entity'
 
 export const config: TypeOrmModuleOptions = {
     type: 'postgres',
-    username: 'intelbras',
-    password: '12345678',
+    username: 'cnailhduhthqvb',
+    password: '2205b6d10e0f0d4f456d370e80241862f7fbab3b519e7ba623f0c2b50d366cb6',
     port: 5432,
-    host: '127.0.0.1',
-    database: 'postgres',
+    host: 'ec2-34-227-120-79.compute-1.amazonaws.com',
+    database: 'd8i8oepqkqvr8p',
     synchronize: true,
     entities: [VideoDevice], 
 }
